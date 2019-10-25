0c754ee1defc46368c45144f4820cc8853dd6ef0
Zoe Christ <zoe@christ.de>
2019-10-25 08:07:26 +0200

    foto bleu getauschht

--- src/database.js
+++ src/database.js
@@ -25,7 +25,7 @@ class Database {
                 link:       "https://de.wikipedia.org/wiki/Petrine",
             },{
                 id:          2,
-                img:        "restaurants/bleu.jpg",
+                img:        "restaurants/bleu.png",
                 name:       "Cafe Bleu",
                 typ:        "Mittag- und Abendessen",
                 gruendungsjahr: 1968,
