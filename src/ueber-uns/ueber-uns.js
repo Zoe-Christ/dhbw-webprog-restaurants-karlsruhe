3c6cdb0e66c3481a159fd2aad946e017589f8184
Zoe Christ <zoe@christ.de>
2019-10-28 10:07:54 +0100

    anpassung ueber uns und index

--- src/ueber-uns/ueber-uns.js
+++ src/ueber-uns/ueber-uns.js
@@ -8,4 +8,30 @@
          this._app = app;
          this._recordId = -1;
          this._data = null;
+
+         async show(matches) {
+             // // URL-Parameter auswerten
+             // this._recordId = matches[1];
+             // this._data = this._app.database.getRecordById(this._recordId);
+
+             // Anzuzeigenden Seiteninhalt nachladen
+             let html = await fetch("ueber-uns/ueber-uns.html");
+             let css = await fetch("ueber-uns/ueber-uns.css");
+
+             if (html.ok && css.ok) {
+                 html = await html.text();
+                 css = await css.text();
+             } else {
+                 console.error("Fehler beim Laden des HTML/CSS-Inhalts");
+                 return;
+             }
+
+             // Seite zur Anzeige bringen
+             let pageDom = this._processTemplate(html);
+
+             this._app.setPageTitle("Ueber uns");
+             this._app.setPageCss(css);
+             this._app.setPageHeader(pageDom.querySelector("header"));
+             this._app.setPageContent(pageDom.querySelector("main"));
+         }
      }
